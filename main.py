0c3964ee098ed8f06bacb996b93d4adf7d73bb89
One Summer can Change Everything Summer <70513990+billa298@users.noreply.github.com>
2022-04-11 06:28:28 +0000

    flask server

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-from pyrogram import Client
+from flask import Flask
+app = Flask('app')
 
-from pytgcalls import idle
-from pytgcalls import PyTgCalls
-from pytgcalls import StreamType
-from pytgcalls.types.input_stream import AudioVideoPiped
-import os
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
 
-app = Client(
-    session_name=os.environ['joma'],
-    api_id=int(os.environ['ab']),
-    api_hash=os.environ['cd'],
-)
-
-call_py = PyTgCalls(app)
-call_py.start()
-video_file = 'test.mkv'
-call_py.join_group_call(
-    -1001547787482,
-    AudioVideoPiped(
-        video_file,
-    ),
-    stream_type=StreamType().pulse_stream,
-)
-idle()
+app.run(host='0.0.0.0', port=8080)
